3466a73942e9d36016ff0825f4578b9c3a699224
Samarth Patrikar <patrikarsamarth@gmail.com>
2023-12-09 19:07:19 +0530

    final

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-const fs = require("fs");
-const { google } = require("googleapis");
 
 const runApplication=require('./runApp');
-
-
-// async function storeToken(token) {
-//   try {
-//     fs.writeFileSync(tokenPath, JSON.stringify(token));
-//     console.log("Token stored to", tokenPath);
-//   } catch (err) {
-//     console.error("Error storing token:", err);
-//   }
-// }
-
-// async function getStoredToken() {
-//   try {
-//     const token = fs.readFileSync(tokenPath);
-//     return JSON.parse(token);
-//   } catch (err) {
-//     return null;
-//   }
-// }
-
 
 // Run the application
 runApplication();
